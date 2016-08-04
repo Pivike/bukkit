c47ab7a6ba6707de4edbb8bc069a673e073869ad
Pivike <pivi.android@gmail.com>
2016-08-04 13:07:29 +0200

    megírtam az error részt

--- minecraft.bat
+++ minecraft.bat
@@ -8,11 +8,11 @@ if[%1] = [] (
 )
 
 :ERROR
-	ECHO SZERVERINFÓK
-	ECHO
-	ECHO
-	ECHO
-	ECHO
+	ECHO A szerver inditasahoz szukseges tennivalok:
+	ECHO 1 - Hozz letre errol a file-ról egy parancsikont az asztelon.
+	ECHO 2 - Az ikonra kattints jobb gombal, es a tulajdonsagait nyitsd meg.
+	ECHO 3 - A "Cel:" mezoben a szoveg moge ird be (egy SPACE utan) a szerver verziot.
+	ECHO 4 - Alkalmazd a valtozasokat, es kattints az ikonra hogyy elinduljon a szerver.
 GOTO END
 
 :1.5.2
